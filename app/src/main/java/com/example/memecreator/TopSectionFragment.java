8b75998502d29a3e1a035d248565c9d52fac2a59
KhalidTouch <khalidhulabaloo9@gmail.com>
2019-09-15 01:13:16 +0100

    integrating an onClickListener

--- app/src/main/java/com/example/memecreator/TopSectionFragment.java
+++ app/src/main/java/com/example/memecreator/TopSectionFragment.java
@@ -1,5 +1,6 @@
 package com.example.memecreator;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,6 +9,7 @@ import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class TopSectionFragment extends Fragment{
@@ -15,12 +17,40 @@ public class TopSectionFragment extends Fragment{
     private static EditText topTextInput;
     private static EditText bottomTextInput;
 
+    TopSectionListener activityCommander;
+
+    public interface TopSectionListener{
+        public void createMeme(String top, String bottom);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try{}
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.top_section_fragment, container, false);
 
-        topTextInput = (EditText)view.findViewById(R.id.)
+        topTextInput = (EditText)view.findViewById(R.id.topTextInput);
+        bottomTextInput = (EditText)view.findViewById(R.id.bottomTextInput);
+        final Button button = (Button)view.findViewById(R.id.button);
+
+        button.setOnClickListener(
+                new View.OnClickListener(){
+                    public void onClick(View v){
+                        buttonClicked(v);
+                    }
+                }
+        );
+
+
         return view;
+    }
+
+    public void buttonClicked(View view){
+
     }
 }
